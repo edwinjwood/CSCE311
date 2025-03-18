63cc8f5f45771dd48758a558bbc218c2dd87778a
Edwin Wood <edwinjwood@gmail.com>
2025-03-17 21:45:18 -0400

    Commit proj 2

---
+++ Project2/proj2/build/bool_expr_client.d
@@ -0,0 +1,6 @@
+build/bool_expr_client.o: src/bool_expr_client.cc \
+ include/bool_expr_client.h ../ipc/include/domain_socket.h \
+ ../util/include/bool_expr_parser.h
+include/bool_expr_client.h:
+../ipc/include/domain_socket.h:
+../util/include/bool_expr_parser.h:
---
+++ Project2/proj2/build/domain_socket.d
@@ -0,0 +1,3 @@
+build/domain_socket.o: ../ipc/src/domain_socket.cc \
+ ../ipc/include/domain_socket.h
+../ipc/include/domain_socket.h:
